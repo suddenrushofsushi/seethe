f3c2b22f47020f87b7b4364a57fd10065d010485
Thomas Hart II <myrridin@gmail.com>
2015-08-05 19:44:56 -0400

    Little bit of method extraction and rearranging

--- lib/seethe/complexity.rb
+++ lib/seethe/complexity.rb
@@ -14,27 +14,13 @@ module Seethe
       @cutoff = flog_cutoff
     end
 
-    def new_template(body, details={format: :html})
-      ActionView::Template.new(body, "irrelevant", details.fetch(:handler) { erb_handler }, details)
-    end
-
-    def process(hash = false)
+    def process
       unless path.is_a? Array
         set_path(Seethe.glob_directory(@path))
       end
 
       totals = path.inject({}) do |totals, file|
-        begin
-          ruby_source = File.read(file)
-          ruby_source = erb_handler.call(new_template(ruby_source)) if file.end_with?(".erb")
-          sexp_parsed = RubyParser.new.parse(ruby_source)
-
-          flog_totals = flog_totals_for(sexp_parsed) 
-          mean = flog_totals.map { |k,v| v }.select { |v| v > cutoff }.mean
-          totals[file] = mean unless mean.nil?
-        rescue Exception => e
-          puts "Error parsing #{file} (#{e.message})"
-        end
+        process_file(file, totals)
         totals
       end
 
@@ -46,14 +32,35 @@ module Seethe
       totals.inject({}) { |memo, ary| memo[ary[0]] = ary[1]; memo }
     end
 
+    private
+
+    def new_template(body, details={format: :html})
+      ActionView::Template.new(body, "irrelevant", details.fetch(:handler) { erb_handler }, details)
+    end
+
+    def process_file(file, totals)
+      begin
+        sexp_parsed = parse_file(file)
+        flog_totals = flog_totals_for(sexp_parsed)
+        mean = flog_totals.values.select { |v| v > cutoff }.mean
+        totals[file] = mean unless mean.nil?
+      rescue Exception => e
+        puts "Error parsing #{file} (#{e.message})"
+      end
+    end
+
+    def parse_file(file)
+      ruby_source = File.read(file)
+      ruby_source = erb_handler.call(new_template(ruby_source)) if file.end_with?(".erb")
+      RubyParser.new.parse(ruby_source)
+    end
+
     def flog_totals_for(sexp)
       flog = Flog.new
       flog.process(sexp)
       flog.calculate_total_scores
       flog.totals
     end
-
-    private
 
     def erb_handler
       @erb_handler ||= ActionView::Template::Handlers::ERB.new
